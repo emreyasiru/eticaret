183bffd3792af2814a6367027897b26b6c24865d
emre <emreyasiru@gmail.com>
2025-08-22 02:41:59 +0300

    up.

--- deneme1/Controllers/HomeController.cs
+++ deneme1/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace deneme1.Controllers
         {
             return View();
         }
+        public IActionResult magaza()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- deneme1/Program.cs
+++ deneme1/Program.cs
@@ -37,7 +37,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Admin}/{action=Giris}/{id?}")
+    pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 
