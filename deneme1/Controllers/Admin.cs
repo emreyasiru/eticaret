ab5d03bb60db9be9122b018e3236aeccea610b22
emre <emreyasiru@gmail.com>
2025-08-22 22:37:18 +0300

    up

--- deneme1/Controllers/Admin.cs
+++ deneme1/Controllers/Admin.cs
@@ -162,5 +162,9 @@ namespace eticaret.Controllers
                 return View();
             }
         }
+        public IActionResult deneme()
+        {
+            return View();
+        }
     }
 }
